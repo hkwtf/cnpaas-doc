4ca9a792d11684b601bffe27e62dc80c7913d606
Jimmy Chu <jimmychu0807@gmail.com>
2014-11-07 20:36:48 +0800

    Updated CNPaaS brand style

--- js/user-login.js
+++ js/user-login.js
@@ -1,10 +1,15 @@
+---
+---
+
 $(function() {
   var setLoggedIn = function(user_slug) {
     $(".user-login-user_slug").text(user_slug);
     $(".user-login").removeClass("user-login-false").addClass("user-login-true");
+    $("a.brand").attr("href", "{{ site.dashboardurl }}")
   }
   var setLoggedOut = function() {
     $(".user-login").removeClass("user-login-true").addClass("user-login-false");
+    $("a.brand").attr("href", "{{ site.parenturl }}")
   }
 
 
