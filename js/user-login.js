beb2ca5532bccb1074464fc3db4bf2850eb59735
LiAh Sheep <me@liah.hk>
2014-11-10 02:10:39 +0800

    Factorise url, add dev URLs support (run `rake server:development` to start local server)

--- js/user-login.js
+++ js/user-login.js
@@ -5,15 +5,15 @@ $(function() {
   var setLoggedIn = function(user_slug) {
     $(".user-login-user_slug").text(user_slug);
     $(".user-login").removeClass("user-login-false").addClass("user-login-true");
-    $("a.brand").attr("href", "{{ site.dashboardurl }}")
+    $("a.brand").attr("href", "{{ site._env.dashboard_url }}")
   }
   var setLoggedOut = function() {
     $(".user-login").removeClass("user-login-true").addClass("user-login-false");
-    $("a.brand").attr("href", "{{ site.parenturl }}")
+    $("a.brand").attr("href", "{{ site._env.www_url }}")
   }
 
 
-  $.getJSON("http://dashboard.cnpaas.io" + "/users/current.js?callback=?", function(user) {
+  $.getJSON("{{ site._env.dashboard_url }}" + "/users/current.js?callback=?", function(user) {
     if (user && (user_slug = user.user_slug)) {
       setLoggedIn(user_slug);
     } else {
