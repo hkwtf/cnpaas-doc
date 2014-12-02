8ca53f0d36c29affb2232407defcd404daaf1bfb
LiAh Sheep <me@liah.hk>
2014-12-03 01:37:52 +0800

    Update user-login logic according to www/portal changes

--- js/user-login.js
+++ js/user-login.js
@@ -5,27 +5,27 @@ $(function() {
   var setLoggedIn = function(user_slug) {
     $(".user-login-user_slug").text(user_slug);
     $(".user-login").removeClass("user-login-false").addClass("user-login-true");
+    $(".user-login-true-controls .user_apps_path").attr("href", user_apps_path(user_slug));
     $("a.brand").attr("href", "{{ site._env.dashboard_url }}")
   }
   var setLoggedOut = function() {
     $(".user-login").removeClass("user-login-true").addClass("user-login-false");
+    $(".user-login-true-controls .user_apps_path").attr("href", "#");
     $("a.brand").attr("href", "{{ site._env.www_url }}")
+  }
+  var user_apps_path = function(user_slug) {
+    return "{{ site._env.dashboard_url }}" + "/u/" + user_slug + "/a";
   }
 
 
-  $.getJSON("{{ site._env.dashboard_url }}" + "/users/current.js?callback=?", function(user) {
-    if (user && (user_slug = user.user_slug)) {
+  $.getJSON("{{ site._env.dashboard_url }}" + "/u/current.js?callback=?", function(user) {
+    if (user) {
+      var user_slug = user.user_slug;
+      $("head").append("<meta name='csrf-param' content='" + user["csrf-param"] + "'/>");
+      $("head").append("<meta name='csrf-token' content='" + user["csrf-token"] + "'/>");
       setLoggedIn(user_slug);
     } else {
       setLoggedOut();
-    }
-
-    var csrfParam, csrfToken;
-    if (csrfParam = user["csrf-param"]) {
-      $("head").append("<meta name='csrf-param' content='" + csrfParam + "'/>");
-    }
-    if (csrfToken = user["csrf-token"]) {
-      $("head").append("<meta name='csrf-token' content='" + csrfToken + "'/>");
     }
 
   }).fail(function () {
