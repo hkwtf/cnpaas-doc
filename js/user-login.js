f2aabbc4a592112a2a11e1be3bf403cf7d5ab364
LiAh Sheep <me@liah.hk>
2014-12-04 03:31:15 +0800

    fix: on the path to access app resources

--- js/user-login.js
+++ js/user-login.js
@@ -5,18 +5,12 @@ $(function() {
   var setLoggedIn = function(user_slug) {
     $(".user-login-user_slug").text(user_slug);
     $(".user-login").removeClass("user-login-false").addClass("user-login-true");
-    $(".user-login-true-controls .user_apps_path").attr("href", user_apps_path(user_slug));
     $("a.brand").attr("href", "{{ site._env.dashboard_url }}")
   }
   var setLoggedOut = function() {
     $(".user-login").removeClass("user-login-true").addClass("user-login-false");
-    $(".user-login-true-controls .user_apps_path").attr("href", "#");
     $("a.brand").attr("href", "{{ site._env.www_url }}")
   }
-  var user_apps_path = function(user_slug) {
-    return "{{ site._env.dashboard_url }}" + "/u/" + user_slug + "/a";
-  }
-
 
   $.getJSON("{{ site._env.dashboard_url }}" + "/u/current.js?callback=?", function(user) {
     if (user) {
